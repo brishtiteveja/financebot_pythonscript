79100c75708484632a77aa12b470afc793e9d21e
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2020-05-12 15:42:15 -0400

    (Forgot to add files in previous commit) coin data import for binance/gdax working nicely. KNN OHLC pattern recognition in progress. Orderbook data saving to sqlite database in progress.

---
+++ run_coin_importer.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+while true; 
+do
+	cmd="exchange_coindata_importer.py"
+	res=$(ps aux | grep -v grep | grep $cmd)
+
+	if [ -z "$res" ] 
+	then
+		echo "Running the importer program again"
+		python3 $cmd > log_coindata_importer.txt &
+	else
+		echo "Already running"
+	fi
+
+	sleep 5
+
+	#sleep 1000
+done
